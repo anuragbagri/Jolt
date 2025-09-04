402919084eb71e4327e78cb56cb7d78e962eb3a2
anurag bagri <anurag123bagri@gmail.com>
2025-09-04 12:59:37 +0530

    primary backend done

--- apps/primary-backend/index.ts
+++ apps/primary-backend/index.ts
@@ -1,12 +1,13 @@
 import {prismaclient } from "db/client";
 import express from "express";
 import cors from "cors";
+import { authMiddleware } from "./middleware";
 
 const app = express();
 app.use(express.json());
 app.use(cors);
 
-app.post("/project", async (req, res) => {
+app.post("/project",authMiddleware, async (req, res) => {
   const { prompt } = req.body;
   const userId = req.userId;
   const description = prompt.split("\n")[0];
@@ -17,9 +18,9 @@ app.post("/project", async (req, res) => {
 });
 
 
-app.get("projects", async (req, res) => {
-    const userId = req.userId;
-    const project = await prismaclient.project.findUnique({
+app.get("projects",authMiddleware, async (req, res) => {
+    const userId = req.userId!;
+    const project = await prismaclient.project.findFirst({
         where : {userId},
     });
     res.json({project});
